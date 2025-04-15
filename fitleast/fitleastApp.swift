6715dac82522f4e14c660863f8886b9fa446c3dd
Ahmet Bugra <abavcilar@gmail.com>
2025-04-15 17:04:47 +0200

    Initial Commit

---
+++ fitleast/fitleastApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitleastApp.swift
+//  fitleast
+//
+//  Created by Ahmet Bugra Avcilar on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct fitleastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fitleastTests/fitleastTests.swift
@@ -0,0 +1,16 @@
+//
+//  fitleastTests.swift
+//  fitleastTests
+//
+//  Created by Ahmet Bugra Avcilar on 15.04.2025.
+//
+
+import Testing
+
+struct fitleastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
